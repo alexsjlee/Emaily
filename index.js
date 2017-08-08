f85219f73f6a3b9dcb11aae1d96f2bc87196f335
alexsjlee <sjlee89@gmail.com>
2017-08-07 21:42:33 -0700

    Changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 9000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 });
 
 app.listen(PORT, () => {
