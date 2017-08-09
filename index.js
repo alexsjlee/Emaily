d8947b52bd58af4282337fe67cc92a88a4cdfb33
alexsjlee <sjlee89@gmail.com>
2017-08-09 16:26:11 -0700

    Finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const PORT = process.env.PORT || 9000;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+); 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 app.listen(PORT, () => {
-    console.log('Server is listening on', PORT);
+  console.log('Server is listening on', PORT);
 });
