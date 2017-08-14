b6f3e3060435221fce27745b70a6087c4bba27cc
alexsjlee <sjlee89@gmail.com>
2017-08-14 16:51:26 -0700

    Added billing and client side app!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -8,19 +9,30 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-); 
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log('Server is listening on', PORT);
